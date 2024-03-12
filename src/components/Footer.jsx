d78f07f7b6b88ac7e76a75d1c0edddc85f696f93
santiagoj <santiago.jimenez.giraldo1@gmail.com>
2024-03-12 15:29:10 -0500

    app para seleccionar lugares para visitar + React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h4>Powered by
+                <a href="https://github.com/SantiagoJimenez04">
+                    <span>@SantiagoJimenez04</span>
+                </a>
+            </h4>
+        </footer>
+    )
+}
